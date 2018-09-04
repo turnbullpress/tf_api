ab9053cbb33f9ffe62cf4fbafad45941b2d9b548
James Turnbull <james@lovedthanlost.net>
2018-09-04 18:19:54 -0400

    Fixed node installation

--- files/api_bootstrap.sh
+++ files/api_bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y nodejs npm
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo ln -s `which nodejs` /usr/bin/node
 sudo npm install -g json-server
 sudo mkdir -p /opt/json-server
